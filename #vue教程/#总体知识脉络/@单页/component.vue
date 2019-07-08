ff6c69349d1b22b53609b29224b3ca1bbcbfc148
Administrator <393502736@qq.com>
2019-07-08 11:12:42 +0800

    ''

---
+++ #vue教程/#总体知识脉络/@单页/component.vue
@@ -0,0 +1,31 @@
+<!--template被vueloader解析为js-->
+
+<template>
+
+
+</template>
+
+
+
+
+
+<script>
+
+    //  vue构造器的配置文件  es6-module输出
+    export default {
+
+
+
+    }
+
+</script>
+
+
+
+
+<!--style被css-loader解析爲js-->
+<style>
+
+
+
+</style>
