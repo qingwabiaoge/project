103fb772d378d840324f1a8b16770023b9ab2ede
Administrator <393502736@qq.com>
2019-07-23 02:24:41 +0800

    Revert "'"
    This reverts commit 03cf8411a42e2cf33f030eeaae09a3ae19d15f65.

--- #js6原生/2对象数据类型/Function自定义的运算符/类/code/code/多态/弱类型不监测类型/2.js
+++ #js6原生/2对象数据类型/Function自定义的运算符/类/code/code/多态/弱类型不监测类型/2.js
@@ -1,19 +1,18 @@
-//超类
 function Animal(sound) {
     this.duckSinging = function () {
         console.log(sound);
     }
 }
 
-//两个类
+
 var duck = new Animal('嘎嘎嘎')
 var chicken = new Animal('嘎嘎嘎')
 
 
 var choir = []; // 合唱团
 
-//一个函数适合不同类
-var joinChoir = function (animal) {//检查动物类型java静态类型是这样做的
+
+var joinChoir = function (animal) {
     if (animal && typeof animal.duckSinging === 'function') {
         choir.push(animal);
         console.log('恭喜加入合唱团');
