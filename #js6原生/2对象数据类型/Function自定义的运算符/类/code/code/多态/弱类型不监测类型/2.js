03cf8411a42e2cf33f030eeaae09a3ae19d15f65
Administrator <393502736@qq.com>
2019-07-23 01:41:03 +0800

    '

--- #js6原生/2对象数据类型/Function自定义的运算符/类/code/code/多态/弱类型不监测类型/2.js
+++ #js6原生/2对象数据类型/Function自定义的运算符/类/code/code/多态/弱类型不监测类型/2.js
@@ -1,18 +1,19 @@
+//超类
 function Animal(sound) {
     this.duckSinging = function () {
         console.log(sound);
     }
 }
 
-
+//两个类
 var duck = new Animal('嘎嘎嘎')
 var chicken = new Animal('嘎嘎嘎')
 
 
 var choir = []; // 合唱团
 
-
-var joinChoir = function (animal) {
+//一个函数适合不同类
+var joinChoir = function (animal) {//检查动物类型java静态类型是这样做的
     if (animal && typeof animal.duckSinging === 'function') {
         choir.push(animal);
         console.log('恭喜加入合唱团');
