c922a7dba52ce29165f167fde0981db44f7ccbaf
Administrator <393502736@qq.com>
2019-08-26 22:21:28 +0800

    ''

--- #node教程/对象/code/@test/1.js
+++ #node教程/对象/code/@test/1.js
@@ -1,23 +1,4 @@
-const Koa = require('koa')
-const app = new Koa()
-const Router = require('koa-router')
-
-
-router = new Router()
-router.get('/', async (ctx, next) => {
-  ctx.body += 'root'
-})
-
-
-app.use(async (ctx, next) => {
-  ctx.body = '1';
-  await next();
-  ctx.body += 2;
-
-})
-  .use(router.routes())
-  .use(router.allowedMethods())
-
-
-app.listen(3000)
-console.log(":3000")
+const fs=require('fs')
+const callback=require('./callback')
+const i=fs.readFile('./cs/1.txt',callback)
+console.log(i)
